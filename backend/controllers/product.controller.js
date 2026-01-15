c3b1d4374fb6c30e71ac128888e95533b7155943
Shrey <shreypanchal936@gmail.com>
2026-01-15 13:30:35 -0500

    Validates product ID and improves error handling on delete
    Prevents deletion attempts with invalid product IDs by adding a validity check, returning a clear error message.
    Updates server error responses to use correct status code for better API reliability.

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.model.js";
+import mongoose from "mongoose";
 
 export const getProducts = async (req, res) => {
   try {
@@ -48,11 +49,15 @@ export const updateProduct = async (req, res) => {
 export const deleteProduct = async (req, res) => {
   const {id} = req.params;
   
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid Product ID" });
+  }
+
   try {
     await Product.findByIdAndDelete(id);
     res.status(200).json({ success: true, message: "Product deleted successfully" });
   } catch (error) {
     console.error("Error in deleting product:", error.message);
-    res.status(404).json({ success: false, message: "Server Error" });
+    res.status(500).json({ success: false, message: "Server Error" });
   }
 }
